4bc4d1b66fba77ec4e4f5c885ee351b0db2f52a2
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-25 09:23:16 +0200

    create add method from Team class

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -7,3 +7,10 @@ test('adding character to team', () => {
   team.add(character);
   expect(team.members).toContain(character);
 });
+
+test('repeated adding a character to team', () => {
+  const character = new Character('Marusia');
+  const team = new Team();
+  team.add(character);
+  expect(team.add(character)).toThrow();
+});
